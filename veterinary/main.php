633fbfd6c960c838bc40dfb6e0f4f74656c426ca
abhikushh <31417271+abhikushh@users.noreply.github.com>
2018-05-01 18:01:43 +0530

    Add files via upload

---
+++ veterinary/main.php
@@ -0,0 +1,125 @@
+
+
+<!DOCTYPE html>
+<html>
+ 
+<head>
+<title>DUVASU , MATHURA</title>
+
+    <style>
+        *{
+            margin:0px;
+            padding:0px;
+        }
+      
+        h1{
+            background-color: v;
+            font-family:Algerian;
+            padding: 15px;
+        }
+       
+        .kush{
+             z-index: 1000;
+             position:absolute;
+            
+       
+        }
+        ul1 ul1{
+            list-style: none;
+        }
+ul1 ul1 ul1
+{
+ margin: 120px;
+ padding: 0px;
+ list-style:none;
+}
+
+ul1 li1
+{
+ float:left;
+ width:261px;
+ height:50;
+ background-color:red;
+ opacity: .70;
+ line-height: 50px;
+ text-align:center;
+ font-size:30px;
+ margin-right:6.5px;
+    z-index: 0;
+    position:absolute;
+    position:relative;
+ 
+}
+ul1 ul1 ul1
+        {
+            margin-left: 120px;
+        }
+ul1 li1 a:hover
+{
+ background-color:blue;
+ display:block;
+}
+
+ul1 li1 ul1 li1
+{
+ display : none;
+}
+
+ul1 li1:hover ul1 li1
+{
+ display : block;
+}
+      
+        a{
+            color: white;
+        }
+        
+        
+    </style>
+</head>
+<body>
+    <div class="abc">
+        <h1 style="color:white;"><center>DUVASU , MATHURA</center></h1>
+    </div>
+    <center>
+    <div class="kush" style="margin:5px;" style="padding:0px;">
+    <ul1>
+    <li1><a href="index.html">HOME</a></li1>
+    <li1><a href="index.html">STUDENTS</a>
+        <ul1>
+            <li1><a href="student/timetable/timetable.html">TIME-TABLE</a></li1>
+                <li1><a href="student/datesheet/datesheet.html">DATESHEET</a></li1>
+                <li1><a href="student/result/result.html">RESULTS</a></li1>
+            <li1><a href="student/sports/sports.html">SPORTS</a></li1>
+            <li1><a href="student/e-tallenge/talent.html">E-TALLENGE</a></li1>
+                </ul1>
+        </li1>
+    <li1><a href="">ADMINISTRATION</a>
+        <ul1>
+            
+            
+               <li1><a href="administration/complaint/colleg.html">COLLEGE</a></li1>
+               <li1><a href="administration/complaint/complaint.html">ADMINISTRATION</a></li1>     
+               <li1><a href="administration/complaint/complainfinal.html">FACULTY</a></li1>
+            
+           
+                <li1><a href="administration/mann_ki_baat/name.html">MANN KI BAAT</a></li1>
+                
+                </ul1>
+        </li1>
+    <li1><a href="">EXPOSURE</a>
+        <ul1>
+            <li1><a href="exposure/todaysarticle.html">ARTICLES</a></li1>
+                <li1><a href="exposure/quiz.html">QUIZ</a></li1>
+                <li1><a href="exposure/eresume/resume.html">RESUME</a></li1>
+                </ul1>
+        </li1>
+    <li1><a href="">CLINIC</a> <ul1>
+            <li1><a href="clinics/gyani/sutta.html">GYNE</a></li1>
+                <li1><a href="clinics/e-reports/report.html">E-REPORTS</a></li1>
+                </ul1></li1></ul1>
+    </div></center>
+    </body>
+    
+   
+</html>
